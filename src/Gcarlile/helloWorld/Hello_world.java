f8bfedbcb0ef4bf10cea29df6cb8b6e27c3c4c62
gabriel.carlile <gabriel.carlile@CLC-LAB209-12.TCSD.tooelesd.org>
2018-09-04 13:22:43 -0600

    first commit

---
+++ src/Gcarlile/helloWorld/Hello_world.java
@@ -0,0 +1,29 @@
+/**
+ * hello world prints hello world in console
+ */
+package Gcarlile.helloWorld;
+
+/**
+ * @author gabriel.carlile
+ * 9/18
+ * hello world
+ * 
+ */ 
+public class Hello_world {
+
+	/**
+	 * 
+	 */
+	public Hello_world() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
